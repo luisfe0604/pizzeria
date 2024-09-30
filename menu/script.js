287a36fe8265cfeac32e77131dd5ea76878edde6
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2024-09-30 16:05:26 -0300

    front reload

--- menu/script.js
+++ menu/script.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://pizzeria-l6im.onrender.com/menu') 
+    fetch('https://pizzeria-l6im.onrender.com/menu')
         .then(response => response.json())
         .then(data => {
             const ul = document.getElementById('pizza-items');
             data.forEach(item => {
                 const li = document.createElement('li');
                 li.innerHTML = `
-                    <div class="name">${item.name} <span class="value">$${item.b}</span> <span class="value">$${item.g}</span> <span class="value">$${item.m}</span> <span class="value">$${item.p}</span></div>
+                    <div class="name">${item.name}
+                        <span class="value">$${Number(item.b).toFixed(2)}</span> 
+                        <span class="value">$${Number(item.g).toFixed(2)}</span> 
+                        <span class="value">$${Number(item.m).toFixed(2)}</span> 
+                        <span class="value">$${Number(item.p).toFixed(2)}</span></div>
                     <div class="ingredients">${item.ingredients}</div>
                 `;
-                ul.appendChild(li);
+                ul.appendChild(li).toFixed(2);
             });
         })
         .catch(error => console.error('Erro ao carregar os itens:', error));
