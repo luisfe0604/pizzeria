c05751419463ec9defe4a446f659050c137d6c09
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2024-10-08 22:13:48 -0300

    updating functions

--- menu/script.js
+++ menu/script.js
@@ -17,4 +17,36 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         })
         .catch(error => console.error('Erro ao carregar os itens:', error));
+
+    fetch('https://pizzeria-l6im.onrender.com/items/portion')
+        .then(response => response.json())
+        .then(data => {
+            const ul = document.getElementById('portion-items');
+            data.forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <div class="name">${item.name}
+                        <span class="value">$${Number(item.value).toFixed(2)}</span> 
+                    </div>
+                `;
+                ul.appendChild(li);
+            });
+        })
+        .catch(error => console.error('Erro ao carregar os itens:', error));
+
+    fetch('https://pizzeria-l6im.onrender.com/items/drink')
+        .then(response => response.json())
+        .then(data => {
+            const ul = document.getElementById('drink-items');
+            data.forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <div class="name">${item.name}
+                        <span class="value">$${Number(item.value).toFixed(2)}</span> 
+                    </div>
+                `;
+                ul.appendChild(li);
+            });
+        })
+        .catch(error => console.error('Erro ao carregar os itens:', error));
 });
